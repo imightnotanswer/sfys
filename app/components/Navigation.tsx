eeff8a9c3a36a9dd111832e1a4dffd332e479e0d
Zach Winans <zach>
2025-04-01 18:26:10 -0400

    Update nav spacing

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -12,7 +12,7 @@ export default function Navigation() {
         <nav className="py-0">
             {/* Desktop/Tablet Navigation */}
             <div className="hidden md:block px-8">
-                <div className="flex flex-wrap gap-x-8 gap-y-1 justify-start">
+                <div className="flex flex-wrap gap-x-8 gap-y-1 -mt-6 justify-start">
                     <Link
                         href="/"
                         className={`text-3xl lg:text-4xl font-[RideSlow] tracking-wide transition-colors ${isActive('/') ? 'text-[#e43720]' : 'hover:text-[#e43720]'}`}

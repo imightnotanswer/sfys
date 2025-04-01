dc523b27ed70580fbab4ef778305ac1d5861cd3b
Zach Winans <zach>
2025-03-31 20:47:29 -0400

    style: update social media icons in footer - Replace Facebook and Instagram icons with new SVG designs - Adjust Facebook icon size and alignment - Maintain hover effects and transitions

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Link from 'next/link'
-import Image from 'next/image'
 import MailingListSignup from './MailingListSignup'
 
 export default function Footer() {
@@ -63,27 +62,29 @@ export default function Footer() {
                         >
                             PRIVACY POLICY
                         </a>
+                        <div className="w-[200px]">
+                            <MailingListSignup isFooter={true} />
+                        </div>
                     </div>
-                    {/* Social Links pushed to right */}
-                    <div className="flex items-center gap-4">
+                    <div className="flex items-center gap-4 ml-auto">
                         <a
-                            href="https://facebook.com/touristswelcome"
+                            href="https://www.facebook.com/touristswelcome"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="hover:text-[#e43720] transition-colors"
+                            className="hover:[&>svg]:text-[#e43720] transition-colors mt-[2px]"
                         >
-                            <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
-                                <path d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4v-8.5z" />
+                            <svg viewBox="0 0 9.309 17.916" width="8" height="16" fill="currentColor" className="text-[#eceadf] transition-colors">
+                                <path d="M8.781,9.744l0.412-3.185H6.039V4.525c0-0.921,0.258-1.552,1.579-1.552h1.69V0.126   C9.013,0.085,8.013,0,6.845,0C4.414,0,2.75,1.484,2.75,4.21v2.349H0v3.185h2.75v8.172h3.289V9.744H8.781z"></path>
                             </svg>
                         </a>
                         <a
-                            href="https://instagram.com/touristswelcome"
+                            href="https://www.instagram.com/tourists_welcome/"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="hover:text-[#e43720] transition-colors"
+                            className="hover:[&>svg]:text-[#e43720] transition-colors mt-[2px]"
                         >
-                            <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
-                                <path d="m2.327 0h13.485c1.278 0 2.329 1.05 2.329 2.33v13.484c0 1.283-1.051 2.328-2.329 2.328h-13.485c-1.281 0-2.327-1.045-2.327-2.328v-13.484c0-1.28 1.046-2.33 2.327-2.33m10.89 2.016c-0.45 0-0.816 0.369-0.816 0.818v1.954c0 0.448 0.366 0.817 0.816 0.817h2.051c0.452 0 0.816-0.369 0.816-0.817v-1.954c0-0.449-0.364-0.818-0.816-0.818h-2.051zm2.873 5.656h-1.596c0.152 0.495 0.235 1.017 0.235 1.558 0 3.018-2.529 5.463-5.644 5.463-3.111 0-5.64-2.445-5.64-5.463 0-0.541 0.084-1.062 0.234-1.558h-1.662v7.664c0 0.398 0.324 0.723 0.72 0.723h12.633c0.4 0 0.721-0.324 0.721-0.723v-7.664zm-7.004-2.171c-2.009 0-3.641 1.583-3.641 3.532 0 1.951 1.632 3.529 3.641 3.529 2.015 0 3.645-1.578 3.645-3.529-1e-3 -1.949-1.63-3.532-3.645-3.532" />
+                            <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor" className="text-[#eceadf] transition-colors">
+                                <path d="m2.327 0h13.485c1.278 0 2.329 1.05 2.329 2.33v13.484c0 1.283-1.051 2.328-2.329 2.328h-13.485c-1.281 0-2.327-1.045-2.327-2.328v-13.484c0-1.28 1.046-2.33 2.327-2.33m10.89 2.016c-0.45 0-0.816 0.369-0.816 0.818v1.954c0 0.448 0.366 0.817 0.816 0.817h2.051c0.452 0 0.816-0.369 0.816-0.817v-1.954c0-0.449-0.364-0.818-0.816-0.818h-2.051zm2.873 5.656h-1.596c0.152 0.495 0.235 1.017 0.235 1.558 0 3.018-2.529 5.463-5.644 5.463-3.111 0-5.64-2.445-5.64-5.463 0-0.541 0.084-1.062 0.234-1.558h-1.662v7.664c0 0.398 0.324 0.723 0.72 0.723h12.633c0.4 0 0.721-0.324 0.721-0.723v-7.664zm-7.004-2.171c-2.009 0-3.641 1.583-3.641 3.532 0 1.951 1.632 3.529 3.641 3.529 2.015 0 3.645-1.578 3.645-3.529-1e-3 -1.949-1.63-3.532-3.645-3.532"></path>
                             </svg>
                         </a>
                     </div>
@@ -91,8 +92,8 @@ export default function Footer() {
 
                 {/* Tablet/Mobile Layout */}
                 <div className="min-[1300px]:hidden flex flex-col items-center gap-4 text-base font-['Gotham Narrow']">
-                    {/* SFYS Contact Info (visible when hidden in header) */}
-                    <div className="min-[1400px]:hidden flex flex-col items-center gap-2 text-[#eceadf]">
+                    {/* SFYS Contact Info */}
+                    <div className="flex flex-col items-center gap-2 text-[#eceadf]">
                         <a
                             href="https://goo.gl/maps/KeGVsBu7MZ52"
                             target="_blank"
@@ -115,21 +116,6 @@ export default function Footer() {
                         >
                             SINGFORYOURSLUMBER@GMAIL.COM
                         </a>
-                    </div>
-
-                    {/* Mailing List - shown when hidden in header */}
-                    <div className="min-[1300px]:hidden flex flex-col items-center gap-4">
-                        <a
-                            href="https://open.spotify.com/user/59e35uquka5jra1pj969r2j4c?si=9486ca3e2e0149fc"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-[10px] leading-[32px] hover:text-[#e43720] transition-colors font-['Gotham Narrow'] font-[600] tracking-[0.15em] uppercase"
-                        >
-                            Tourists Radio
-                        </a>
-                        <div className="w-[200px]">
-                            <MailingListSignup isFooter={true} />
-                        </div>
                     </div>
 
                     {/* Footer Links */}
@@ -166,26 +152,31 @@ export default function Footer() {
                         </a>
                     </div>
 
-                    {/* Social Links */}
+                    {/* Mailing List */}
+                    <div className="w-[200px]">
+                        <MailingListSignup isFooter={true} />
+                    </div>
+
+                    {/* Social Media Icons */}
                     <div className="flex items-center gap-4">
                         <a
-                            href="https://facebook.com/touristswelcome"
+                            href="https://www.facebook.com/touristswelcome"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="hover:text-[#e43720] transition-colors"
+                            className="hover:[&>svg]:text-[#e43720] transition-colors mt-[2px]"
                         >
-                            <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
-                                <path d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4v-8.5z" />
+                            <svg viewBox="0 0 9.309 17.916" width="8" height="16" fill="currentColor" className="text-[#eceadf] transition-colors">
+                                <path d="M8.781,9.744l0.412-3.185H6.039V4.525c0-0.921,0.258-1.552,1.579-1.552h1.69V0.126   C9.013,0.085,8.013,0,6.845,0C4.414,0,2.75,1.484,2.75,4.21v2.349H0v3.185h2.75v8.172h3.289V9.744H8.781z"></path>
                             </svg>
                         </a>
                         <a
-                            href="https://instagram.com/touristswelcome"
+                            href="https://www.instagram.com/tourists_welcome/"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="hover:text-[#e43720] transition-colors"
+                            className="hover:[&>svg]:text-[#e43720] transition-colors mt-[2px]"
                         >
-                            <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
-                                <path d="m2.327 0h13.485c1.278 0 2.329 1.05 2.329 2.33v13.484c0 1.283-1.051 2.328-2.329 2.328h-13.485c-1.281 0-2.327-1.045-2.327-2.328v-13.484c0-1.28 1.046-2.33 2.327-2.33m10.89 2.016c-0.45 0-0.816 0.369-0.816 0.818v1.954c0 0.448 0.366 0.817 0.816 0.817h2.051c0.452 0 0.816-0.369 0.816-0.817v-1.954c0-0.449-0.364-0.818-0.816-0.818h-2.051zm2.873 5.656h-1.596c0.152 0.495 0.235 1.017 0.235 1.558 0 3.018-2.529 5.463-5.644 5.463-3.111 0-5.64-2.445-5.64-5.463 0-0.541 0.084-1.062 0.234-1.558h-1.662v7.664c0 0.398 0.324 0.723 0.72 0.723h12.633c0.4 0 0.721-0.324 0.721-0.723v-7.664zm-7.004-2.171c-2.009 0-3.641 1.583-3.641 3.532 0 1.951 1.632 3.529 3.641 3.529 2.015 0 3.645-1.578 3.645-3.529-1e-3 -1.949-1.63-3.532-3.645-3.532" />
+                            <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor" className="text-[#eceadf] transition-colors">
+                                <path d="m2.327 0h13.485c1.278 0 2.329 1.05 2.329 2.33v13.484c0 1.283-1.051 2.328-2.329 2.328h-13.485c-1.281 0-2.327-1.045-2.327-2.328v-13.484c0-1.28 1.046-2.33 2.327-2.33m10.89 2.016c-0.45 0-0.816 0.369-0.816 0.818v1.954c0 0.448 0.366 0.817 0.816 0.817h2.051c0.452 0 0.816-0.369 0.816-0.817v-1.954c0-0.449-0.364-0.818-0.816-0.818h-2.051zm2.873 5.656h-1.596c0.152 0.495 0.235 1.017 0.235 1.558 0 3.018-2.529 5.463-5.644 5.463-3.111 0-5.64-2.445-5.64-5.463 0-0.541 0.084-1.062 0.234-1.558h-1.662v7.664c0 0.398 0.324 0.723 0.72 0.723h12.633c0.4 0 0.721-0.324 0.721-0.723v-7.664zm-7.004-2.171c-2.009 0-3.641 1.583-3.641 3.532 0 1.951 1.632 3.529 3.641 3.529 2.015 0 3.645-1.578 3.645-3.529-1e-3 -1.949-1.63-3.532-3.645-3.532"></path>
                             </svg>
                         </a>
                     </div>
